87146ba32c679ad306951703937e8dd29aa8a708
Raj3448 <rajchavan6659@gmail.com>
2024-10-05 20:46:52 +0530

    fixes

--- lib/ui/home/components/news_component.dart
+++ lib/ui/home/components/news_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newshub/core/constant/font_text.dart';
+import 'package:newshub/ui/home/pages/news_article_page.dart';
 import 'package:newshub/view_models/news_model.dart';
 
 class NewsComponent extends StatelessWidget {
@@ -8,56 +9,61 @@ class NewsComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xFFF5F5F5),
-          borderRadius: BorderRadius.circular(10)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 30,
-                    child: Text(
-                      article.title ?? '',
-                      style: AppFonts.bodyFont
-                          .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewsDetailsViewPage(article: article, isFav: false,),));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xFFF5F5F5),
+            borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      child: Text(
+                        article.title ?? '',
+                        style: AppFonts.bodyFont.copyWith(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  SizedBox(
-                    height: 70,
-                    child: Text(
-                      article.description ?? '',
-                      style: AppFonts.bodyFont,
+                    const SizedBox(
+                      height: 5,
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 70,
+                      child: Text(
+                        article.description ?? '',
+                        style: AppFonts.bodyFont,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, right: 10, bottom: 10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              clipBehavior: Clip.antiAlias,
-              child: Image.network(
-                  fit: BoxFit.cover,
-                  width: 110,
-                  height: 110,
-                  article.urlToImage ??
-                      'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg'),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, right: 10, bottom: 10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                clipBehavior: Clip.antiAlias,
+                child: Image.network(
+                    fit: BoxFit.cover,
+                    width: 110,
+                    height: 110,
+                    article.urlToImage ??
+                        'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/ui/home/pages/news_article_page.dart
@@ -0,0 +1,235 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:newshub/core/constant/app_colors.dart';
+import 'package:newshub/core/constant/font_text.dart';
+import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
+import 'package:newshub/view_models/news_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// ignore: must_be_immutable
+class NewsDetailsViewPage extends StatefulWidget {
+  final Article article;
+  bool isFav;
+  NewsDetailsViewPage({
+    Key? key,
+    required this.article,
+    required this.isFav,
+  }) : super(key: key);
+
+  @override
+  State<NewsDetailsViewPage> createState() => _NewsDetailsViewPageState();
+}
+
+class _NewsDetailsViewPageState extends State<NewsDetailsViewPage>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late AnimationController _savedController;
+  bool isSaved = false;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    _savedController = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+    if (widget.isFav) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _savedController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              size: 30,
+              color: AppColors.primaryColor,
+            )),
+        centerTitle: true,
+        title: Text('News Details', style: AppFonts.titleFont),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              isSaved = !isSaved;
+
+              if (isSaved) {
+                _savedController.forward();
+              } else {
+                _savedController.reverse();
+              }
+            },
+            child: LottieBuilder.asset(
+              controller: _savedController,
+              'assets/animations/saved.json',
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.network(
+                          widget.article.urlToImage!,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: CustomPaint(
+                              size: const Size(15, 15),
+                              painter: DotPainter(color: Colors.black),
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.article.author!.length > 45
+                                    ? 'Raj Chavan'
+                                    : widget.article.author!,
+                                style: AppFonts.bodyFont.copyWith(
+                                    color: AppColors.primaryColor,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14),
+                              ),
+                              Text(
+                                DateFormat('E, dd/MM/yyyy')
+                                    .format(widget.article.publishedAt),
+                                style: AppFonts.bodyFont.copyWith(
+                                    color: AppColors.primaryColor,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          GestureDetector(
+                            onTap: () {
+                              if (widget.isFav) {
+                                
+                                _controller.forward();
+                              } else {
+                                _controller.reverse();
+                              }
+                              setState(() {
+                                widget.isFav = !widget.isFav;
+                              });
+                            },
+                            child: LottieBuilder.asset(
+                              'assets/animations/favorite.json',
+                              height: 65,
+                              fit: BoxFit.cover,
+                              controller: _controller,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        widget.article.title!,
+                        style: AppFonts.bodyFont
+                          .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      widget.article.description!,
+                      style: AppFonts.bodyFont,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom:  30.0),
+            child: ElevatedButton(
+                onPressed: _launchURL,
+                style: const ButtonStyle(
+                    fixedSize: WidgetStatePropertyAll(Size(300, 70)),
+                    backgroundColor:
+                        WidgetStatePropertyAll(AppColors.primaryColor)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Read More  ',
+                      style: AppFonts.buttonFont.copyWith(color: Colors.white),
+                    ),
+                    LottieBuilder.asset(
+                      'assets/animations/rocket.json',
+                      height: 50,
+                    )
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+
+  _launchURL() async {
+    String url = widget.article.url!;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+class DotPainter extends CustomPainter {
+  final Color color;
+
+  DotPainter({required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
